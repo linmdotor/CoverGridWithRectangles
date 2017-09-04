8000f967e35212aff88187c60785cbf46e1c48b3
linmdotor <linmdotor@gmail.com>
2017-09-04 15:02:16 +0200

    Added partial (non optimal) solution

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <conio.h>
 #include <vector>
+#include <algorithm>
 #include <iostream>
 using namespace std;
 
@@ -34,9 +35,9 @@ void Print2DVector(const vector<vector<int>> &grid)
     {
         for (int j = 0; j < grid[i].size(); j++)
         {
-            cout << grid[i][j] << " ";
+            std::cout << grid[i][j] << " ";
         }
-        cout << endl;
+        std::cout << endl;
     }
 }
 
@@ -48,21 +49,78 @@ void PrintRectangle(const rectangle &rect, const int &gridHeight, const int &gri
         {
             if ((rect.corner1.x <= j && rect.corner1.y <= i) &&
                 (rect.corner2.x >= j && rect.corner2.y >= i))
-                cout << "0 "; // prints '0' as part of the rectangle
+                std::cout << "0 "; // prints '0' as part of the rectangle
             else
-                cout << "- ";
+                std::cout << "- ";
 
         }
-        cout << endl;
+        std::cout << endl;
     }
 }
 
+bool CurrentRectIsOpen(const coord2D &_rect)
+{
+    return (_rect.x != -1 && _rect.y != -1);
+}
+
+bool CellIsOccupied(const vector<vector<int>> &initialGrid, const coord2D &_pos)
+{
+    return (initialGrid[_pos.y][_pos.x] == 1);
+}
+
+bool PartialRectangleIsCorrect(const vector<vector<int>> &initialGrid, coord2D _currentRectInit, coord2D _currentRectEnd)
+{
+    for (int i = _currentRectInit.x; i <= _currentRectEnd.x; i++)
+    {
+        for (int j = _currentRectInit.y; j <= _currentRectEnd.y; j++)
+        {
+            if (CellIsOccupied(initialGrid, coord2D(i, j)))
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void MarkPartialRectangleOccupied(vector<vector<int>> &currentGrid, coord2D _currentRectInit, coord2D _currentRectEnd)
+{
+    //mark the currentRect as occupied
+    for (int i = _currentRectInit.y; i <= _currentRectEnd.y; i++)
+    {
+        for (int j = _currentRectInit.x; j <= _currentRectEnd.x; j++)
+        {
+            currentGrid[i][j] = 1;
+        }
+    }
+}
+
+bool IsGridComplete(const vector<vector<int>> &initialGrid)
+{
+    for (int i = 0; i < initialGrid[0].size(); i++)
+    {
+        for (int j = 0; j < initialGrid.size(); j++)
+        {
+            if (!CellIsOccupied(initialGrid, coord2D(i, j)))
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool IsValidSolution(const vector<vector<int>> &initialGrid, const coord2D &_currentRect)
+{
+    return (!CurrentRectIsOpen(_currentRect) && IsGridComplete(initialGrid));
+}
 ////////////////////////////////////////////////////////////////////////////////
 // ALGORITHM
 ////////////////////////////////////////////////////////////////////////////////
-int CalculateRectanglesRecursive(const vector<vector<int>> &initialGrid, vector<rectangle> &solution,
-        coord2D _currentPos, coord2D _currentRectangle, int _currentNumRect, vector<rectangle> _currentSolution)
+int CalculateRectanglesRecursive(vector<vector<int>> &initialGrid, vector<rectangle> &solution,
+        coord2D _currentPos, coord2D _currentRect, int &_currentNumRect, vector<rectangle> _currentSolution)
 {
+    ////////////////////////////////////////////////////////////////////////////////
     // We are trying to solve the problem with a backtracking algorithm
 
     // We will go through the whole triangle, and in each cell we will "decide" what recursion take;
@@ -74,14 +132,105 @@ int CalculateRectanglesRecursive(const vector<vector<int>> &initialGrid, vector<
 
     //We will evaluate all the possibilities, and will take the best of all.
     //  (with min. number of rectangles)
+    ////////////////////////////////////////////////////////////////////////////////
 
-    return 0;
+    // IS VALID?
+    // If the current cell is out of bounds, this partial solution doesn't work
+    if (_currentPos.y >= initialGrid.size() ||
+        _currentPos.x >= initialGrid[0].size())
+    {
+        return 0;
+    }
+    // If the current cell is occupied, and the currentRect is opened, this partial solution doesn't work
+    if (CurrentRectIsOpen(_currentRect) &&
+        CellIsOccupied(initialGrid, _currentPos))
+    {
+        return 0;
+    }
+
+    // PREPARE LEVEL ITERATION
+    // "Open" a new rectangle if possible when then current is closed (rectangle(-1,-1))
+    if (!CurrentRectIsOpen(_currentRect) &&
+        !CellIsOccupied(initialGrid, _currentPos))
+    {
+        _currentRect = _currentPos;
+    }
+
+    // IS SOLUTION?
+    //Evaluate if we have reached the end (_currentPos == lower-Right position of the grid)
+    if (_currentPos.y == initialGrid.size() - 1 &&
+        _currentPos.x == initialGrid[0].size() - 1)
+    {
+        //Close the current rect if it was opened
+        if (CurrentRectIsOpen(_currentRect) &&
+            !CellIsOccupied(initialGrid, _currentPos))
+        {
+            _currentSolution.push_back(rectangle(_currentRect, _currentPos));
+
+            _currentNumRect++;
+            _currentRect = coord2D(-1, -1);
+        }
+
+        // IS VALID SOLUTION?
+        // We have a solution only if the _currentRect has been closed and all the grid is occupied
+        if(IsValidSolution(initialGrid, _currentRect))
+        {
+            std::cout << "RESULT: " << _currentNumRect << " rectangles." << endl;
+            for each (rectangle rect in _currentSolution)
+            {
+                PrintRectangle(rect, initialGrid.size(), initialGrid[0].size());
+                std::cout << endl;
+            }
+            return _currentNumRect;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    else // BACKTRACKING (k+1)
+    {
+        // Take one of the recursive possibilities:
+
+        // OPTIONS 1 & 2 - MOVE
+        CalculateRectanglesRecursive(initialGrid, solution, coord2D(_currentPos.x + 1, _currentPos.y), _currentRect, _currentNumRect, _currentSolution);
+        CalculateRectanglesRecursive(initialGrid, solution, coord2D(_currentPos.x, _currentPos.y + 1), _currentRect, _currentNumRect, _currentSolution);
+
+        // OPTIONS 1 & 2 - CLOSE THE CURRENT RECTANGLE (IF CORRECT) AND MOVE
+        if (CurrentRectIsOpen(_currentRect) &&
+            !CellIsOccupied(initialGrid, _currentPos))
+        {
+
+            if (PartialRectangleIsCorrect(initialGrid, _currentRect, _currentPos))
+            {
+                _currentSolution.push_back(rectangle(_currentRect, _currentPos));
+                solution.push_back(rectangle(rectangle(_currentRect, _currentPos)));
+
+                MarkPartialRectangleOccupied(initialGrid, _currentRect, _currentPos);
+
+                _currentNumRect++;
+                _currentRect = coord2D(-1, -1);
+
+                CalculateRectanglesRecursive(initialGrid, solution, coord2D(_currentPos.x + 1, _currentPos.y), _currentRect, _currentNumRect, _currentSolution);
+                CalculateRectanglesRecursive(initialGrid, solution, coord2D(_currentPos.x, _currentPos.y + 1), _currentRect, _currentNumRect, _currentSolution);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        return _currentNumRect;
+    }
 }
 
 int CalculateRectangles(const vector<vector<int>> &initialGrid, vector<rectangle> &solution)
 {
-    return CalculateRectanglesRecursive(initialGrid, solution,
-        coord2D(0,0), coord2D(-1,-1), 0, vector<rectangle>());
+    vector<vector<int>> copyGrid = initialGrid;
+    int numRects = 0;
+    CalculateRectanglesRecursive(copyGrid, solution,
+        coord2D(0,0), coord2D(-1,-1), numRects, vector<rectangle>());
+    return numRects;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -97,21 +246,21 @@ int main()
         { 0,0,0,1,1,1 }
     };
 
-    cout << "INITIAL GRID:" << endl;
+    std::cout << "INITIAL GRID:" << endl;
     Print2DVector(initialGrid);
-    cout << endl;
+    std::cout << endl;
 
     vector<rectangle> solution;
     int numRects = CalculateRectangles(initialGrid, solution);
 
-    cout << "RESULT: " << numRects << " rectangles." << endl;
+    std::cout << "RESULT: " << numRects << " rectangles." << endl;
     for each (rectangle rect in solution)
     {
         PrintRectangle(rect, initialGrid.size(), initialGrid[0].size());
-        cout << endl;
+        std::cout << endl;
     }
 
-    cout << "Press any key to Exit...";
+    std::cout << "Press any key to Exit...";
     getch();
     return 0;
 }
